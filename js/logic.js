32c4a5e7018795a01b0467daebc6bbda2d3ff4c0
NaveenWebDev <ns222280@gmail.com>
2023-09-15 13:55:50 +0530

    js logic is ready and now code is generated by entering text

--- js/logic.js
+++ js/logic.js
@@ -1,4 +1,13 @@
 const url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example";
 
-const qrboxcode = document.querySelector("#qrboxcode");
-const qrcode = document.querySelector("#qrcodeimage");
+const qrBoxCode = document.querySelector("#qrboxcode");
+const qrImage = document.querySelector("#qrcodeimage");
+const qrText = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", ()=>{
+    if(qrText.value.length > 0){
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    }
+})
+
