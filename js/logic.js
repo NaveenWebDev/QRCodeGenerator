f6d79a290d4526ce28b91db9c8c44c96fb210e4e
NaveenWebDev <ns222280@gmail.com>
2023-09-15 13:43:58 +0530

    api added

---
+++ js/logic.js
@@ -0,0 +1,4 @@
+const url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example";
+
+const qrboxcode = document.querySelector("#qrboxcode");
+const qrcode = document.querySelector("#qrcodeimage");
